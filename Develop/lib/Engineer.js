2bcbae449bc2c118cf982f521b33bfa04a8e9a72
Robert Harris <rlh9@rice.edu>
2020-03-02 17:29:34 -0600

    Set modules for different employee classes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 var Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name,id,email,gitHub) {
+        super(name,id,email);
+        this.gitHub = gitHub; 
+      }
+    getRole(){
+        return "Engineer";
+    }
+    getGithub(){
+        return this.gitHub;
+    }
+}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,2 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 var Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name,id,email,school) {
+        super(name,id,email);
+        this.school = school; 
+      }
+    getRole(){
+        return "Intern";
+    }
+    getSchool(){
+        return this.school;
+    }
+}
